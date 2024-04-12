d4a1d916299f32aa259a9b05549f8f5ebdcdb657
quyenpq.work@gmail.com <quyenpq.work@gmail.com>
2024-04-12 09:43:03 +0700

    fix: remove unused code in tailwind.config + package.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "@styles/index.scss";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+let rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  rootElement = document.createElement("div");
+  rootElement.id = "root";
+  document.body.appendChild(rootElement);
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
