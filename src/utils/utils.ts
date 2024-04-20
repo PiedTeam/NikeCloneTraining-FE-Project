3f01b760fea5f0e44d7c4b1e9f7dfacedcac9cec
quyenpq.work@gmail.com <quyenpq.work@gmail.com>
2024-04-20 11:24:24 +0700

    feature: verify account UI

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,3 +11,13 @@ export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
   // eslint-disable-next-line import/no-named-as-default-member
   return axios.isAxiosError(error);
 }
+
+export const validateEmail = (email: string) => {
+  const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+};
+
+export const validatePhoneNumber = (phoneNumber: string) => {
+  const regex = /^\d{10}$/;
+  return regex.test(phoneNumber);
+};
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 const viteConfig: UserConfig = {
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 4000,
     watch: {
       usePolling: true,
     },
