e9d2fa64af7334e88256a790a68ef7fe327ad835
Pham Quoc Quyen <137786692+quyn2904@users.noreply.github.com>
2024-04-27 09:38:08 +0700

    feature: verify account UI (#36)
    * feature: verify account UI
    
    * fix: change port to 3000

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 const Register = lazy(() => import("@pages/Register"));
 const Homepage = lazy(() => import("@pages/Homepage"));
 const Login = lazy(() => import("@pages/Login"));
+const VerifyAccount = lazy(() => import("@pages/VerifyAccount"));
 
 const queryClient = new QueryClient();
 
@@ -27,6 +28,7 @@ function App() {
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/verify-account" element={<VerifyAccount />} />
           {/* <Route path="*" element={<Navigate to="/404" />} /> */}
         </Routes>
       </NextUIProvider>
