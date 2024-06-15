e475fc1b412ac5c1f19ca81acd5a088ec571310a
quyenpq.work@gmail.com <quyenpq.work@gmail.com>
2024-06-15 19:18:22 +0700

    hotfix: redirect when change password successfully

--- src/pages/ChangePassword.tsx
+++ src/pages/ChangePassword.tsx
@@ -8,6 +8,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { toast } from "react-toastify";
 import { FormDataChangePasswordApi } from "@services/users.api.ts";
 import usersService from "@services/users.service.ts";
+import { useNavigate } from "react-router-dom";
 
 export interface FormDataChangePassword {
   oldPassword: string;
@@ -31,7 +32,7 @@ const schema = yup.object().shape({
 });
 
 const ChangePassword = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const [accessToken, setAccessToken] = useState("");
   const [isVisible, setIsVisible] = React.useState(false);
   const toggleVisibility = () => setIsVisible(!isVisible);
@@ -65,7 +66,7 @@ const ChangePassword = () => {
       toast.error(error.response.data.data.old_password);
     } else {
       toast.success(message as string);
-      // setTimeout(() => navigate("/"), 3000);
+      setTimeout(() => navigate("/"), 3000);
     }
   };
 
@@ -167,7 +168,7 @@ const ChangePassword = () => {
             className="t-0 mt-4"
             isBlock
             showAnchorIcon
-            href="#"
+            href="/"
             color="primary"
           >
             Back
@@ -178,7 +179,7 @@ const ChangePassword = () => {
             className="relative mb-4 mt-4 overflow-visible rounded-full bg-black px-12 text-white shadow-xl  hover:-translate-y-1"
             onClick={handleChangePasswordButtonClick}
           >
-            Change
+            Submit
           </Button>
         </div>
       </div>
